a99a76fc3d70297697280c4a03724491b31cf6df
Vishal Chaudhary <vishh8630@gmail.com>
2022-07-19 00:43:40 +0530

    To Do Application

---
+++ todo_app/base/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('task', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['complete']
+    
